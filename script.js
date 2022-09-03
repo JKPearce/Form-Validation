7486b7ca677b977172f2e6bcbebf017a53d7c9e2
Jaydon <pearcejaydon@gmail.com>
2022-09-03 15:53:52 +1000

    add onblur error checking for each input field

--- script.js
+++ script.js
@@ -1,8 +1,53 @@
 const emailInput = document.getElementById("email");
+const countryInput = document.getElementById("country");
+const zipCodeInput = document.getElementById("zipCode");
+const passwordInput = document.getElementById("password");
+const confirmPasswordInput = document.getElementById("passwordConfirm");
 
-emailInput.onblur = testEmailInput;
+emailInput.onblur = () => {
+  if (emailInput.checkValidity()) {
+    document.getElementById("emailError").innerText = "";
+  } else {
+    //use the htmlt5 validation message
+    document.getElementById("emailError").innerText =
+      emailInput.validationMessage;
+  }
+};
 
-function testEmailInput() {
-  if (emailInput.checkValidity()) return;
-  emailInput.reportValidity();
-}
+countryInput.onblur = () => {
+  if (countryInput.checkValidity()) {
+    document.getElementById("countryError").innerText = "";
+  } else {
+    //use user defined title in html input
+    document.getElementById("countryError").innerText = countryInput.title;
+  }
+};
+
+zipCodeInput.onblur = () => {
+  if (zipCodeInput.checkValidity()) {
+    document.getElementById("zipCodeError").innerText = "";
+  } else {
+    //use user defined title in html input
+    document.getElementById("zipCodeError").innerText = zipCodeInput.title;
+  }
+};
+
+passwordInput.onblur = () => {
+  if (passwordInput.checkValidity()) {
+    document.getElementById("passwordInputError").innerText = "";
+  } else {
+    //use user defined title in html input
+    document.getElementById("passwordInputError").innerText =
+      passwordInput.title;
+  }
+};
+
+confirmPasswordInput.onblur = () => {
+  console.log(confirmPasswordInput.value);
+  if (confirmPasswordInput.value === passwordInput.value) {
+    document.getElementById("confirmPasswordInputError").innerText = "";
+  } else {
+    document.getElementById("confirmPasswordInputError").innerText =
+      "Passwords do not match";
+  }
+};
